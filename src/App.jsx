b9aa14c482edb0c24eb3a58563790d0728b862ef
anjeh24 <junez24@yahoo.com>
2024-08-22 11:26:50 -0400

    got search bar to work though it does so only when I hit enter in vscode. Working on straightening that out

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,21 @@ import React from 'react'
 import {useState, useEffect} from 'react';
 import "./App.CSS";
 import SearchBar from './components/SearchBar';
-import Cards from './components/Cards';
+
 import ReactDOM from 'react-dom';
 
 
 function App() {
-  const [recipe, setRecipe] = useState([]);
-
- useEffect(() => {
- async function getRecipe() {
-  try {
-    const res = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
-    const data = await res.json();
-    console.log(data);
-    
-    setRecipe(data.meals || []); //setting the new values/state of recipe to objects
-  }catch (error) {
-    console.error('could not fetch recipes', error);
-  }
   
- }
-getRecipe()
-
-}, []);
 
   return (
-    
-    <div>
-      <h1>Meal Prep Helper</h1>
-    
-   {recipe.map((item, index) => (
-    <div className="card_items" key={index}>
-        <img src={item.strMealThumb} alt="food" style={{width:'100%'}}/>
-      <div className="container">
-        <h4><b>{item.strMeal}</b></h4>
-        <a href={'https://www.themealdb.com/meal/' + item.idMeal} target='_blank' rel="noopener noreferrer">Get Recipe & Instructions</a>
+    <>
+      <div>
+        <SearchBar/>
+     
       </div>
-    </div>
-    ))}
-  </div>
-   
-  
+     
+    </>
   )
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from "react";
 
 function SearchBar() {
-  return (
-    <div>
-        <input type='text' value={searchRecipe} onChange={showNewInput} placeholder='Search recipes here'/>
-    </div>
-  )
-}
 
-export default SearchBar
+  const [query, setQuery] = useState("");
+  const[recipe, setRecipe] = useState([]);
+  
+  const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+  const fetchUrl = url + query;
+  useEffect(() => {
+  
+      async function getRecipe() {
+          try {
+              
+            const res = await fetch(fetchUrl);
+            const data = await res.json();
+            console.log(data);
+            
+           setRecipe(data.meals || []); //setting the new values/state of recipe to objects
+          }catch (error) {
+            console.error('could not fetch recipes', error);
+          }
+          
+         }
+    getRecipe();
+  }, []);
+  
+  
+    return (
+      <div>
+      <h2>Search Recipe</h2>
+  
+         <div className="search-btn">
+         
+          <input type="text" placeholder="Find recipe" onChange={evt => setQuery(evt.target.value)}/>
+  
+         </div>
+  
+         {recipe.map((item, index) => (
+      <div className='container_div'>
+      <div className="card_items" key={index}>
+          <img src={item.strMealThumb} alt="food" style={{width:'100%'}}/>
+        <div className="container">
+          <h4><b>{item.strMeal}</b></h4>
+          <a href={'https://www.themealdb.com/meal/' + item.idMeal} target='_blank' rel="noopener noreferrer">Get Recipe & Instructions</a>
+        </div>
+      </div>
+      </div>
+      ))}
+  
+      </div>
+    )
+  }
+  
+  export default SearchBar
